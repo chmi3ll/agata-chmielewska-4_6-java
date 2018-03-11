932382d7729693482aff028a191793b893afed1e
Agata <chmielewska.agata33@gmail.com>
2018-03-11 13:08:58 +0100

    dodanie klasy Kalkulator

--- src/main/java/com/kodilla/zadanie_4_6/Kalkulator.java
+++ src/main/java/com/kodilla/zadanie_4_6/Kalkulator.java
@@ -1,4 +1,20 @@
 package com.kodilla.zadanie_4_6;
 
 public class Kalkulator {
+
+    public void dodawanie(double pierwsza, double druga) {
+        double wynik = pierwsza + druga;
+        System.out.println(wynik);
+    }
+
+    public void odejmowanie(double pierwsza, double druga) {
+        double wynik = pierwsza - druga;
+        System.out.println(wynik);
+    }
+
+
+    public static void main(String args[]) {
+        Kalkulator kalkulator = new Kalkulator();
+        kalkulator.dodawanie(5, 6);
+    }
 }
